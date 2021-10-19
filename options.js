5d9164e5ac67fbbdb1212fe2978055b98eabf581
Cimbali <me@cimba.li>
2021-10-19 21:05:39 +0200

    Rebuild included values from checkboxes every time

--- options.js
+++ options.js
@@ -1,15 +1,19 @@
-browser.storage.local.get('include').then(({ include }) => {
+function update() {
+	const accumulator = (acc, checkbox) => Object.assign(acc, {[checkbox.name]: checkbox.checked})
+	const include = Array.from(document.getElementsByTagName('input')).reduce(accumulator, {})
+
+	if (Object.values(include).reduce((acc, val) => acc + val) >= 2) {
+		browser.storage.local.set({ include });
+		browser.runtime.sendMessage({ include });
+		document.getElementById('more-values-needed').style.display = 'none';
+	} else {
+		document.getElementById('more-values-needed').style.display = 'block';
+	}
+}
+
+browser.storage.local.get({ include: {} }).then(({ include }) => {
 	Array.from(document.getElementsByTagName('input')).forEach(checkbox => {
 		checkbox.checked = include[checkbox.name];
-		checkbox.onclick = event => {
-			include[checkbox.name] = checkbox.checked
-			if (Object.values(include).reduce((v, a) => v + a, 0) >= 2) {
-				browser.storage.local.set({ include });
-				browser.runtime.sendMessage({ include });
-				document.getElementById('more-values-needed').style.display = 'none';
-			} else {
-				document.getElementById('more-values-needed').style.display = 'block';
-			}
-		}
+		checkbox.onclick = update;
 	});
 });
